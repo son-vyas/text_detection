74e8fce67f5bdc92113a4e7fa3cae3b743d63e53
son-vyas <soniyavyas29@gmail.com>
2018-11-10 16:06:47 +0530

    Added code

--- detect.py
+++ detect.py
@@ -2,3 +2,9 @@ import cv2
 import sys
 import pytesseract
 from matplotlib import pyplot as plt
+
+if __name__ == '__main__':
+
+  if len(sys.argv) < 2:
+    print('Usage: python ocr_simple.py image.jpg')
+    sys.exit(1)
